aa1c9e80e7f78e8ee9a93cc929708dcaf6fcf319
Circle-layer-org <circledeveloper3@gmail.com>
2025-07-18 17:48:03 +0100

    Add theme-aware pie chart

---
+++ src/components/ThemeAwarePieChart.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { useColorMode } from '@docusaurus/theme-common';
+
+const ThemeAwarePieChart = ({ className, ...props }) => {
+    const { colorMode } = useColorMode();
+
+    // Choose the appropriate video source based on the current theme
+    const videoSrc = colorMode === 'dark'
+        ? '/img/Pie_Chart_Dark.webm'
+        : '/img/Pie_Chart_Light.webm';
+
+    return (
+        <video
+            src={videoSrc}
+            autoPlay
+            loop
+            muted
+            playsInline
+            className={className}
+            alt="Circle Layer Token Distribution Chart"
+            {...props}
+        >
+            Your browser does not support the video element.
+        </video>
+    );
+};
+
+export default ThemeAwarePieChart; 
